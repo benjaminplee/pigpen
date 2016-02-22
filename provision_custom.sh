acb542355589033343b43f5b36fb14cd16db0044
Benjamin P Lee <benjaminplee@gmail.com>
2016-02-22 08:56:14 -0600

    Setup simple object path utility to test out its selectors

--- provision_custom.sh
+++ provision_custom.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 echo Performing custom task provisioning
+
+sudo apt-get install python python-pip -y
+
+sudo pip install objectpath
+
+echo 'function query() { for f in /vagrant/data/*.json; do objectpath -e "$1" $f ; done }' >> ~/.bashrc
