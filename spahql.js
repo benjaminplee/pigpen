e96945dd37c9b08925a0609f95222731f90fa9cc
ASY Papa Johns Dev Team <papajohns_dev@asynchrony.com>
2016-02-25 09:26:12 -0600

    Cleanup

--- spahql.js
+++ spahql.js
@@ -8,13 +8,10 @@ var file = args[0];
 var selector = args[1];
 
 var data = JSON.parse(fs.readFileSync(file));
-
 var db = SpahQL.db(data);
 
 var selection = db.select(selector);
-
 var values = selection.values();
-
 
 console.log(JSON.stringify(values));
 
